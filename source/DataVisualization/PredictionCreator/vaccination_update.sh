92f8123218fe10ab2d3b7e2bd4c6fd4e122a8b1f
Nate18464 <46988876+Nate18464@users.noreply.github.com>
2021-09-02 19:14:35 -0700

    Delete source directory

--- source/DataVisualization/PredictionCreator/vaccination_update.sh
+++
@@ -1,7 +0,0 @@
-git init
-git pull https://github.com/owid/covid-19-data.git
-git restore public
-cp ./public/data/vaccinations/vaccinations.csv ../../../resource/DataVisualization/
-rm -rf public
-rm -rf scripts
-rm -f README.md
